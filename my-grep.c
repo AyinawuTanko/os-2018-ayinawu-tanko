4d84b5644af6ccd4f9f7b4cae0880dd0c98ee28b
AyinawuTanko <35471739+AyinawuTanko@users.noreply.github.com>
2018-09-12 10:14:49 -0700

    Update my-grep.c

--- my-grep.c
+++ my-grep.c
@@ -1,37 +1,54 @@
 
+#include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
 
 int main(int argc, char *argv[])
 {
 
-        FILE *file;
-        char lines[100];
+	if(argc==1){
+		exit(1);
+	}
+	else{
+		for(int i=1;i<argc;i++){
+        	FILE *file;
+        	char lines[500];
 
-        //Reading the file pointer
-        file = fopen(argv[2], "r");
-        if(file==NULL){
-                printf("my-grep:searchterm [file...]\n");
-        }
+       		 //Reading the file pointer
+       		 file = fopen(argv[2], "r");
+        	if(argc==0){
+               		printf("my-grep:searchterm [file...]");
+        	}
+		if(argc==1){
+			printf("my-grep:cannot open file");
+		}
 
         // Comparing the words in each line by reading the lines
-        while(fscanf(file, "%[^\n]\n",lines)!=EOF)
-        {
-                if(strstr(lines, argv[1])==NULL)
-                {
-                        printf(" \n");
-                }
-                if(strstr(lines, argv[1]) !=NULL)
-                {
+       		 while(fgets(lines,500,file)!=	NULL)
+        	{
+                	if(strstr(lines, argv[1])==NULL)
+                	{
+                        	printf(" ");
+                	}
+                	if(strstr(lines, argv[1]) !=NULL)
+                	{
 
-                        printf("%s\n",lines);
-                }
-                else
-                {
-                        continue;
-                }
-        }
+                        	printf("%s\n",lines);
+                	}
+                	else
+                	{
+                        	continue;
+                	}
+        	}
+	
+
 
          fclose(file);
+	}
 }
+	return 0;
+}
+
+
+
 
