99b3c3663547d34eeb18387a14b5ef87f6b88089
AyinawuTanko <35471739+AyinawuTanko@users.noreply.github.com>
2018-09-13 12:43:18 +0000

    Update my-cat.c

--- my-cat.c
+++ my-cat.c
@@ -1,18 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
+// The main program have as parameters argc and argv
+// argv is a one dimensional array of strings
+// argc contains the number of aguments passed to the program
 int main(int argc, char *argv[])
 {
+        // If the name of the file is invoked without a file
+        // exit
         if(argc==1){
                 exit(1);
         }
-
+        
+        // else loop through the files and print its contents
         else{
 
                 for(int i=1; i<argc;i++){
                         FILE *file;
                         char lines[500];
 
-
+                        // Opens the files and reads them
                         file = fopen(argv[i], "r");
 
                         if(file==NULL){
@@ -23,10 +29,14 @@ int main(int argc, char *argv[])
                         else{
 
                                 printf("Output of my-cat command\n");
+                                
+                                // gets the contents of the files and prints them
                                    while(fgets(lines,500,file)!=NULL){
                                         printf("%s\n", lines);
                                 }
                                 }
+                                
+                                // Closes the file(s)
                                 fclose(file);
 
                 }
